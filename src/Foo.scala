1ca5a756d397467415f88a13959539eb691de2a2
SkyeAv <skye.a.goetz@gmail.com>
2026-08-19 13:18:39 -0700

    test: updating to scala3 bracketless syntax

--- src/Foo.scala
+++ src/Foo.scala
@@ -2,15 +2,12 @@ package foo
 import scalatags.Text.all.*
 import mainargs.{main, Parser}
 
-object Foo {
-  def generateHtml(text: String) = {
+object Foo
+  def generateHtml(text: String) =
     h1(text).toString
-  }
 
   @main
-  def main(text: String) = {
+  def main(text: String) =
     println(generateHtml(text))
-  }
 
   def main(args: Array[String]): Unit = Parser(this).runOrExit(args)
-}
